9c7698dfa9a3b7ab0efcd66854d6a0d0bf5c3571
Mouhamadou Gaye <mouhagaye2015@gmail.com>
2019-06-03 16:21:35 +0000

    ORM

--- store/models.py
+++ store/models.py
@@ -2,16 +2,32 @@ from django.db import models
 
 # Create your models here.
 
-ARTISTS = {
-  'francis-cabrel': {'name': 'Francis Cabrel'},
-  'lej': {'name': 'Elijay'},
-  'rosana': {'name': 'Rosana'},
-  'maria-dolores-pradera': {'name': 'María Dolores Pradera'},
-}
+class Artist(models.Model):
+  name = models.CharField(max_length=200, unique=True)
+  
+  def __str__(self):
+    return self.name
 
+class Contact(models.Model):
+  name = models.CharField(max_length=200)
+  email = models.EmailField(max_length=100)
 
-ALBUMS = [
-  {'name': 'Sarbacane', 'artists': [ARTISTS['francis-cabrel']]},
-  {'name': 'La Dalle', 'artists': [ARTISTS['lej']]},
-  {'name': 'Luna Nueva', 'artists': [ARTISTS['rosana'], ARTISTS['maria-dolores-pradera']]}
-]
+  def __str__(self):
+    return self.name
+
+class Album(models.Model):
+  reference = models.IntegerField(null=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  available = models.BooleanField(default=True)
+  title = models.CharField(max_length=200)
+  picture = models.URLField()
+  artists = models.ManyToManyField(Artist, related_name="albums", blank=True)
+
+  def __str__(self):
+    return self.title
+
+class Booking(models.Model):
+  created_at = models.DateTimeField(auto_now_add=True)
+  contacted = models.BooleanField(default=False)
+  contact = models.ForeignKey(Contact,on_delete=models.CASCADE)
+  album = models.OneToOneField(Album, on_delete=models.CASCADE)
--- store/views.py
+++ store/views.py
@@ -1,33 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import ALBUMS
+from .models import Artist, Album, Booking, Contact
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Salut tout le monde")
+    albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
+    formatted_album = ["<li>{}</li>".format(album) for album in albums]
+    message = "<ul>{}</ul>".format("\n".join(formatted_album))
+    template = loader.get_template('store/index.html')
+    return HttpResponse(template.render(request=request))
+
 def listing(request):
-    albums = ['<li>{}</li>'.format(album['name']) for album in ALBUMS]
-    message = """<ul>{}</ul>""".format("\n".join(albums))
+    albums = Album.objects.filter(available=True)
+    formatted_album = ["<li>{}</li>".format(album) for album in albums]
+    message = "<ul>{}</ul>".format("\n".join(formatted_album))
     return HttpResponse(message)
+
 def details(request, album_id):
     id = int(album_id)
-    album = ALBUMS[id]
-    artists = " ".join([artist['name'] for artist in album['artists']])
-    message = f"Cette album se nomme {album['name']} et a ete ecrit par {artists}"
+    album = Album.objects.get(pk=album_id)
+    artists = " ".join([artist.name for artist in album.artists.all()])
+    message = f"Cette album se nomme {album.title} et a ete ecrit par {artists}"
     return HttpResponse(message)
 def search(request):
     query = request.GET.get('query')
     if not query:
-        message = "Aucun artiste n'est demande"
+        message= "Vous n'avez rien entre"
     else:
-        albums = [
-            album for album in ALBUMS
-            if  query in " ".join(artist['name'] for artist in album['artists'])
-        ]
-        if len(albums) == 0:
-            message = "Nous n'avons trouve aucun album"
+        albums = Album.objects.filter(title__icontains=query)
+        if not albums.exists():
+            albums = Album.objects.filter(artists__name__icontains=query)
+        if not albums.exists():
+            message = "Nous n'avons trouve aucun album correspondant"
         else:
-            albums = ["<li>"+album['name']+"</li>" for album in albums]
+            albums = ["<li>"+album.title+"</li>" for album in albums]
             message = """Les albums correspondantes sont :
             <ul>
                 {}
